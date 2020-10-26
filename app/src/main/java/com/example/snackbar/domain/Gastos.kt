4cfe9cf23f9ca288643c0a33c52969de6192a0bb
TiliaSNogueira <tiliasalomao@gmail.com>
2020-10-26 11:13:11 -0300

    exercicio em grupo aula

---
+++ app/src/main/java/com/example/snackbar/domain/Gastos.kt
@@ -0,0 +1,10 @@
+package com.example.snackbar.domain
+
+import java.util.*
+
+data class Gastos (var descricao: String, var categoria: String, var data: String, var valor: Double  ){
+
+    override fun toString(): String {
+        return "Gastos(descricao='$descricao', categoria='$categoria', data='$data', valor=$valor)"
+    }
+}
